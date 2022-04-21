fbd84ac6e3f44fcdfbd6af1ad997a96e837b9cbc
spacemanspiff-713 <87157301+spacemanspiff-713@users.noreply.github.com>
2022-04-21 15:30:01 -0500

    Header Component Finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react"
+import img from "./images/smiling-face.png"
 
 export default function App(){
   return (
       <nav class="header">
-          <h2>Meme Generator</h2>
+          <div class="left-nav">
+            <img src={img} alt="smiling face" class="header--image"/>
+            <h2>Meme Generator</h2>  
+          </div>
           <p>React Course</p>
       </nav>
       
