6116e285a73e51279ecded1d80c7ac9dca57b99a
spacemanspiff-713 <87157301+spacemanspiff-713@users.noreply.github.com>
2022-04-21 14:37:44 -0500

    First component created (Header) and CSS linked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from "react"
+import React from "react";
+import './App.css';
+import Header from "./components/Header";
 
 export default function App(){
-  return <h1>Hello World!</h1>
+  return <>
+    <Header />
+  </>
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function App(){
+  return (
+      <nav class="header">
+          <h2>Meme Generator</h2>
+          <p>React Course</p>
+      </nav>
+      
+  )
+  
+}
