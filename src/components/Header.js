d1f4b1338a36c3baef0add01b6c199732ad38f5b
spacemanspiff-713 <87157301+spacemanspiff-713@users.noreply.github.com>
2022-04-22 10:14:46 -0500

    generic update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import img from "./images/smiling-face.png"
 
 export default function App(){
   return (
-      <nav class="header">
-          <div class="left-nav">
-            <img src={img} alt="smiling face" class="header--image"/>
+      <nav className="header">
+          <div className="left-nav">
+            <img src={img} alt="smiling face" className="header--image"/>
             <h2>Meme Generator</h2>  
           </div>
           <p>React Course</p>
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,18 @@
 import React from "react"
 
 export default function Meme(){
+    function darken(){
+        console.log("clicked")
+    }
     return (
-        <div class="container">
+        <div className="container">
             <form>
-                <div class="form--inputs">
+                <div className="form--inputs">
                     <input type="text" name="first_input" placeholder="Enter the first text" />
                     <input type="text" name="second_input" placeholder="Enter the second text" />
                 </div>
-                <div class="form--button">
-                    <button>Get A New Meme Image</button>
+                <div className="form--button">
+                    <button onClick={darken}>Get A New Meme Image</button>
                 </div>
                
             </form>
