806c7135673eb62f0447cb21595b722579cfe2f1
spacemanspiff-713 <87157301+spacemanspiff-713@users.noreply.github.com>
2022-04-25 14:03:42 -0500

    Image div and section initiated and basic styles applied

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,21 +1,33 @@
 import React from "react"
+import memesData from "../meme-data.js"
+
 
 export default function Meme(){
-    function darken(){
-        console.log("clicked")
+    // console.log(memes)
+    let memeList = memesData.data.memes
+    let memeDisplay = []
+    for(let i = 0; i < memeList.length; i++){
+        memeDisplay.push(<li>{memeList[i].name}</li>)
     }
+    let rando = Math.floor(Math.random() * memeList.length)
+    let memeImg = memeList[rando].url
     return (
         <div className="container">
-            <form>
                 <div className="form--inputs">
                     <input type="text" name="first_input" placeholder="Enter the first text" />
                     <input type="text" name="second_input" placeholder="Enter the second text" />
                 </div>
                 <div className="form--button">
-                    <button onClick={darken}>Get A New Meme Image</button>
+                    <button>Get A New Meme Image</button>
                 </div>
-               
-            </form>
+                <section className="memeSection">
+                <div className="memeImageWapper" style={{
+                    backgroundImage: `url(${memeImg})`, 
+                    }}>
+                    {/* <img src={memeImg} /> */}
+                </div>
+                </section>
+                
         </div>
     )
 }
